af34a75edd3249cd20f34da7a43050c085e4e36f
fraggod@coercion <mk.fraggod@gmail.com>
2009-12-16 17:46:15 +0500

    Split non-fgc into separate module

--- ipy/impz.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-
-import itertools as it, operator as op, functools as ft
-
-from time import time, sleep, gmtime, mktime, localtime, strftime, strptime, asctime
-from calendar import timegm
-from od import do
