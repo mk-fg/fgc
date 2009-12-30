d8f127e7b4e3f5fcb1fc50e5a7c68a0f241c5a52
fraggod@coercion <mk.fraggod@gmail.com>
2009-12-30 19:53:24 +0500

    strcaps: added process manipulation capabilities

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
 	name = 'fgc',
-	version = '9.12.24',
+	version = '9.12.25',
 	author = 'Mike Kazantsev',
 	author_email = 'mike_kazantsev@fraggod.net',
 	description = ( 'Miscellaneous tools to soften stdlib shortcomings'
--- strcaps.c
+++ strcaps.c
@@ -5,7 +5,7 @@
 
 
 static PyObject *
-strcaps_get(PyObject *self, PyObject *args) { // (int) fd / (str) path / (file) file
+strcaps_get_file(PyObject *self, PyObject *args) { // (int) fd / (str) path / (file) file
 	PyObject *file;
 	if (!PyArg_ParseTuple(args, "O", &file)) return NULL;
 	cap_t caps;
@@ -26,12 +26,34 @@ strcaps_get(PyObject *self, PyObject *args) { // (int) fd / (str) path / (file) 
 	return Py_BuildValue("s#", strcaps, strcaps_len); }; // (str) caps
 
 static PyObject *
-strcaps_set(PyObject *self, PyObject *args) { //  (int) fd / (str) path / (file) file, (str) caps
-	PyObject *file; char *strcaps;
-	if (!PyArg_ParseTuple(args, "Os", &file, &strcaps)) return NULL;
+strcaps_get_process(PyObject *self, PyObject *args) { // (int) pid or None
+	PyObject *pid;
+	if (!PyArg_ParseTuple(args, "|O", &pid)) return NULL;
+	cap_t caps;
+	if (PyObject_Not(pid)) caps = cap_get_proc();
+	else {
+		if (PyInt_Check(pid)) caps = cap_get_pid(PyInt_AsLong(pid));
+		else {
+			PyErr_SetString( PyExc_TypeError,
+				"Expecting process pid as integer or None" );
+			return NULL; } }
+	int strcaps_len; char *strcaps;
+	if (caps == NULL) {
+		if (errno == ENODATA) { strcaps = "\0"; strcaps_len = 0; }
+		else {
+			PyErr_SetString(PyExc_OSError, strerror(errno));
+			return NULL; } }
+	else strcaps = cap_to_text(caps, &strcaps_len);
+	return Py_BuildValue("s#", strcaps, strcaps_len); }; // (str) caps
+
+
+static PyObject *
+strcaps_set_file(PyObject *self, PyObject *args) { // (str) caps, (int) fd / (str) path / (file) file
+	char *strcaps; PyObject *file;
+	if (!PyArg_ParseTuple(args, "sO", &strcaps, &file)) return NULL;
 	cap_t caps;
 	if ((caps = cap_from_text(strcaps)) == NULL) {
-		PyErr_SetString(PyExc_ValueError, "Invalid capabilities specification");
+		PyErr_SetString(PyExc_ValueError, "Invalid capability specification");
 		return NULL; }
 	int err;
 	if (PyString_Check(file)) err = cap_set_file(PyString_AsString(file), caps);
@@ -46,13 +68,39 @@ strcaps_set(PyObject *self, PyObject *args) { //  (int) fd / (str) path / (file)
 		return NULL; }
 	Py_RETURN_NONE; };
 
+static PyObject *
+strcaps_set_process(PyObject *self, PyObject *args) { // (str) caps, (int) pid or None
+	char *strcaps; PyObject *pid;
+	if (!PyArg_ParseTuple(args, "s|O", &strcaps, &pid)) return NULL;
+	cap_t caps;
+	if ((caps = cap_from_text(strcaps)) == NULL) {
+		PyErr_SetString(PyExc_ValueError, "Invalid capability specification");
+		return NULL; }
+	int err;
+	if (PyObject_Not(pid)) err = cap_set_proc(caps);
+	else {
+		if (PyInt_Check(pid)) err = capsetp(PyInt_AsLong(pid), caps);
+		else {
+			PyErr_SetString( PyExc_TypeError,
+				"Expecting process pid as integer or None" );
+			return NULL; } }
+	if (err) {
+		PyErr_SetString(PyExc_OSError, strerror(errno));
+		return NULL; }
+	Py_RETURN_NONE; };
+
 
 
 static PyMethodDef strcaps_methods[] = {
-	{"get", strcaps_get, METH_VARARGS,
+	{"get_file", strcaps_get_file, METH_VARARGS,
 		"Get capabilities string from path, file or descriptor.\n"},
-	{"set", strcaps_set, METH_VARARGS,
+	{"get_process", strcaps_get_process, METH_VARARGS,
+		"Get capabilities string from pid or current thread, if pid is omitted.\n"},
+	{"set_file", strcaps_set_file, METH_VARARGS,
 		"Set capabilities from string to a path, file or descriptor.\n"},
+	{"set_process", strcaps_set_process, METH_VARARGS,
+		"Set capabilities from string for a pid or current thread, if pid is omitted,"
+		" setting caps for an arbitrary pid is deprecated and might be unsupported.\n"},
 	{NULL, NULL, 0, NULL} };
 
 PyDoc_STRVAR( strcaps__doc__,
