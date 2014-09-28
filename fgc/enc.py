f9a951c80c283fbd84658cd17ef3cefc675a495a
Mike Kazantsev <mk.fraggod@gmail.com>
2014-09-28 14:58:21 +0600

    enc: +force_bytes +force_unicode +to_bytes

--- fgc/enc.py
+++ fgc/enc.py
@@ -1,4 +1,8 @@
-import sys
+#-*- coding: utf-8 -*-
+from __future__ import print_function
+
+import sys, io, types
+
 enc_default = sys.getdefaultencoding()
 if enc_default in ('ascii', 'ANSI_X3.4-1968'): enc_default = 'utf-8' # ascii is always bad idea
 
@@ -8,8 +12,6 @@ def dec(string, enc=None):
 	else: return unicode(bytes(string), enc or enc_default)
 
 
-from cStringIO import StringIO as sio
-
 def get(src):
 	src_enc = None
 	sets = (
@@ -17,7 +19,7 @@ def get(src):
 		('koi8-r', xrange(0xc0, 0xdf), 0),
 		('cp1251', xrange(0xe0, 0xff), 0) )
 	encs = dict([(enc[0], 0) for enc in sets])
-	if isinstance(src, str): src = sio(src)
+	if isinstance(src, str): src = io.BytesIO(src)
 	src.seek(0)
 	chr = ord(src.read(1))
 	while chr:
@@ -37,3 +39,18 @@ def get(src):
 			except TypeError: pass
 		if not src_enc: src_enc = sorted(encs, key=lambda x: encs[x], reverse=True)[0]
 	return src_enc
+
+
+def force_bytes(bytes_or_unicode, encoding=None, errors='backslashreplace'):
+	encoding = encoding or enc_default
+	if isinstance(bytes_or_unicode, bytes): return bytes_or_unicode
+	return bytes_or_unicode.encode(encoding, errors)
+
+def force_unicode(bytes_or_unicode, encoding=None, errors='replace'):
+	encoding = encoding or enc_default
+	if isinstance(bytes_or_unicode, unicode): return bytes_or_unicode
+	return bytes_or_unicode.decode(encoding, errors)
+
+def to_bytes(obj, **conv_kws):
+	if not isinstance(obj, types.StringTypes): obj = bytes(obj)
+	return force_bytes(obj)
